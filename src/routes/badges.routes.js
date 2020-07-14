5958f48adca8c8598672bb15174f145babff222f
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-07-13 22:24:01 -0500

    Re-done the project because compability issues with the use of create-react-app

--- src/routes/badges.routes.js
+++ src/routes/badges.routes.js
@@ -3,14 +3,32 @@ const router = express.Router();
 const Badge = require('../models/badge');
 
 router.get('/', async (req,res)=>{
-        
-        try{
-           const badges = await Badge.find()
-           console.log(badges)
-           res.json(badges)
-        }catch(error){
+        const badges = await Badge.find()
+        res.json(badges)
+});
 
-        }
+router.get('/:id',async (req,res)=>{
+        const badge = await Badge.findById(req.params.id);
+        res.json(badge);
+});
+
+router.post('/',async (req,res)=>{
+        const {firstName , lastName , jobTitle , twitter} = req.body;
+        const badge = new Badge({firstName,lastName,jobTitle,twitter});
+        await badge.save();
+        res.json({status : "Added"})
+});
+
+router.put('/:id',async (req,res)=>{
+        const {firstName , lastName , jobTitle , twitter} = req.body;
+        const newBadge = {firstName , lastName , jobTitle , twitter};
+        await Badge.findByIdAndUpdate(req.params.id,newBadge);
+        res.json({status : "Updated"});
+});
+
+router.delete('/:id',async(req,res)=>{
+        await Badge.findByIdAndDelete(req.params.id);
+        res.json({status : "Deleted"});
 });
 
 module.exports = router;
